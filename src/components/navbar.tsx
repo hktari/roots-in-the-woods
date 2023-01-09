ebfe75d3978544dcb148f1b7b363d04caef700a5
hktari <bkamnik1995@gmail.com>
2023-01-09 13:28:30 +0100

    remove tickets page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ const NavBar = (props: Props) => {
     <nav>
         <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/tickets">Tickets</a></li>
+            <li><a href="https://fienta.com/roots-in-the-woods-2023">Tickets</a></li>
             <li><a href="/news">News</a></li>
         </ul>
     </nav>
