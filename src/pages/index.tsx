4d0c32427d4d4e20602ba8ee8bef8438f3faf36c
hktari <bkamnik1995@gmail.com>
2023-01-11 11:25:57 +0100

    style section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,39 @@ import { graphql, HeadFC, PageProps } from "gatsby"
 import Layout from "../components/layout/layout";
 import PostItem from "../components/post-item";
 
+import aboutImg from '../images/festival-night1.jpeg';
+import aboutImgTwo from '../images/stereo-black-white.jpg';
+
 const IndexPage = ({ data }: PageProps<Queries.IndexPageQuery>) => {
 
   return (
     <Layout>
-      <button className="btn btn-primary btn-lg">hello</button>
 
-      <i className="bi-alarm"></i>
-      {data?.allContentfulPost?.nodes?.map(post => (<PostItem key={post.id} post={post} />))}
+      <section className="o-section c-about">
+        <h2 className="o-section__title">Reggae and Dub Festival</h2>
+
+        <div className="row">
+          <div className="o-section__col">
+            <p className="o-section__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni possimus nobis odio, voluptate vel enim dolorem dolores non vitae rem quos provident quas itaque nesciunt quibusdam mollitia, quisquam eveniet.</p>
+          </div>
+          <div className="o-section__col o-section_col--full-width-mobile">
+            <img src={aboutImg} alt="" className="o-section__img" />
+          </div>
+          <div className="o-section__col">
+            <p className="o-section__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magni possimus nobis odio, voluptate vel enim dolorem dolores non vitae rem quos provident quas itaque nesciunt quibusdam mollitia, quisquam eveniet.</p>
+          </div>
+          <div className="o-section__col o-section_col--full-width-mobile">
+            <img src={aboutImgTwo} alt="" className="o-section__img" />
+          </div>
+        </div>
+
+      </section>
+
+      <br />
+      <br />
+      <br />
+
+
     </Layout>
   )
 }
--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
+import { graphql, HeadFC, PageProps } from "gatsby"
+import PostItem from '../components/post-item'
+
 
 type Props = {}
 
-const NewsPage = (props: Props) => {
+const NewsPage = ({ data }: PageProps<Queries.NewsPageQuery>) => {
     return (
         <Layout>
-            <>
-            news page
-            </>
+            {data?.allContentfulPost?.nodes?.map(post => (<PostItem key={post.id} post={post} />))}
+
         </Layout>)
 }
 
 export default NewsPage
+
+
+export const query = graphql`
+  query NewsPage {
+    allContentfulPost {
+      nodes {
+        id
+        title
+        node_locale
+        text {
+          raw
+        }
+        thumbnail {
+          id
+          url
+          title
+        }
+        gallery {
+          id
+          url
+        }
+      }
+    }
+}`
