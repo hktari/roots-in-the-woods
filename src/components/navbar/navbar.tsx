e37809345c817ac4ff4916f8d6631c89e8c9e8c4
hktari <bkamnik1995@gmail.com>
2023-01-16 11:38:41 +0100

    navbar logo is link and padding

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,13 +12,13 @@ const NavBar = ({ openMenuClicked }: NavBarProps) => {
       {/* <h1 className='c-navbar__title'>
         Roots in the Woods
       </h1> */}
-      <div className="c-logo c-navbar__logo">
+      <Link to='/' className="c-logo c-navbar__logo">
         <img width="64px" height="64px" src={logo} alt="roots in the woods logo" />
-      </div>
+      </Link>
       <button className="c-navbar__menu-toggle" onClick={() => openMenuClicked()}><i className="bi bi-list"></i></button>
       <nav className='c-navbar__menu'>
         <ul className='c-navbar__menu-list'>
-          <li className='c-navbar__menu-list-item'><Link activeClassName='c-link--active' className='c-link'  to="/">Home</Link></li>
+          <li className='c-navbar__menu-list-item'><Link activeClassName='c-link--active' className='c-link' to="/">Home</Link></li>
           <li className='c-navbar__menu-list-item'><a className='c-link' href="https://fienta.com/roots-in-the-woods-2023">Tickets</a></li>
           <li className='c-navbar__menu-list-item'><Link activeClassName='c-link--active' className='c-link' to="/events">Events</Link></li>
           <li className='c-navbar__menu-list-item'><Link activeClassName='c-link--active' className='c-link' to="/news">News</Link></li>
