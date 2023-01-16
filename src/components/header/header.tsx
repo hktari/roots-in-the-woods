ea8029dbe62ced4f2c76cb3dcbf13dcdf0065502
hktari <bkamnik1995@gmail.com>
2023-01-16 08:53:08 +0100

    add config.scss and sidebar files

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import NavBar from '../navbar/navbar'
 import headerImage from '../../images/roots-banner-2023.jpg'
+import SideBar from '../sidebar'
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
-        <header className='c-header'>
-            <img className='c-header__img' src={headerImage} alt="" />
-            <NavBar />
-        </header>
+        <>
+            <header className='c-header'>
+                <img className='c-header__img' src={headerImage} alt="" />
+                <NavBar />
+            </header>
+            <SideBar />
+        </>
     )
 }
 
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const SideBar = (props: Props) => {
+  return (
+    <div className='c-sidebar'>
+        
+    </div>
+  )
+}
+
+export default SideBar
