4c9b98fe6b61c72a9cfdfac5e925088ad2cf9180
hktari <bkamnik1995@gmail.com>
2023-01-16 10:21:50 +0100

    replace a with Link inside navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from "gatsby"
 import logo from '../../images/logo.jpg';
 
 type NavBarProps = {
@@ -18,10 +18,10 @@ const NavBar = ({openMenuClicked}: NavBarProps) => {
       <button className="c-navbar__menu-toggle" onClick={() => openMenuClicked()}><i className="bi bi-list"></i></button>
       <nav className='c-navbar__menu'>
         <ul className='c-navbar__menu-list'>
-          <li className='c-navbar__menu-list-item'><a href="/">Home</a></li>
-          <li className='c-navbar__menu-list-item'><a href="https://fienta.com/roots-in-the-woods-2023">Tickets</a></li>
-          <li className='c-navbar__menu-list-item'><a href="/events">Events</a></li>
-          <li className='c-navbar__menu-list-item'><a href="/news">News</a></li>
+          <li className='c-navbar__menu-list-item'><Link to="/">Home</Link></li>
+          <li className='c-navbar__menu-list-item'><Link to="https://fienta.com/roots-in-the-woods-2023">Tickets</Link></li>
+          <li className='c-navbar__menu-list-item'><Link to="/events">Events</Link></li>
+          <li className='c-navbar__menu-list-item'><Link to="/news">News</Link></li>
         </ul>
       </nav>
     </div>
