a4532d96f3cacec0452eddfcce562860926d829b
hktari <bkamnik1995@gmail.com>
2023-01-16 10:34:55 +0100

    close sidebar when link is clicked

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from "gatsby"
+import { useRef } from 'react'
+import { useEffect } from 'react'
 
 type SideBarProps = {
   isOpen: boolean
@@ -8,6 +10,31 @@ type SideBarProps = {
 
 const SideBar = ({ isOpen, closeMenuClicked }: SideBarProps) => {
 
+  const menuListRef = useRef<HTMLElement>()
+
+  useEffect(() => {
+
+    if (!menuListRef.current) {
+      return;
+    }
+
+    const links = menuListRef.current.querySelectorAll('.c-sidebar__menu-list-item > a')!
+
+    const linkClickEventHandler = () => closeMenuClicked()
+
+    links.forEach(link => {
+      console.log('link', link.getAttribute('href'))
+
+      // close menu when link is clicked
+      link.addEventListener('click', linkClickEventHandler)
+    })
+
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', linkClickEventHandler)
+      })
+    }
+  }, [menuListRef])
 
   return (
     <div className={`c-sidebar ${isOpen ? 'c-sidebar--show' : ''}`}>
@@ -18,7 +45,7 @@ const SideBar = ({ isOpen, closeMenuClicked }: SideBarProps) => {
       </header>
 
       <nav className='c-sidebar__menu'>
-        <ul className='c-sidebar__menu-list'>
+        <ul className='c-sidebar__menu-list' ref={menuListRef}>
           <li className='c-sidebar__menu-list-item'><Link to="/">Home</Link></li>
           <li className='c-sidebar__menu-list-item'><a href="https://fienta.com/roots-in-the-woods-2023">Tickets</a></li>
           <li className='c-sidebar__menu-list-item'><Link to="/events">Events</Link></li>
