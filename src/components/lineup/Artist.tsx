e4b9255b6bcf84bbb564e242d2697af9218956e0
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-06-11 12:29:38 +0200

    extract LineupArtist.tsx

---
+++ src/components/lineup/Artist.tsx
@@ -0,0 +1,83 @@
+import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
+import React from "react";
+
+function findImageForArtist(
+  artist: Queries.AllArtistsPageQuery["allArtistsJson"]["nodes"][0],
+  images: Queries.AllArtistsPageQuery["allImageSharp"]["nodes"]
+): [IGatsbyImageData, string] {
+  const imageName = artist.images?.[0];
+
+  if (!imageName) {
+    throw new Error(`Artist ${artist?.id} has no image assigned `);
+  }
+
+  const imageNode = images.find((image) =>
+    imageName.includes(image.fixed?.originalName!)
+  );
+
+  if (!imageNode?.gatsbyImageData) {
+    throw new Error(`Failed to find image with name ${imageName}`);
+  }
+
+  return [imageNode.gatsbyImageData, imageName];
+}
+
+const _ArtistLinkType = {
+  FB: "Facebook",
+  SOUNDCLOUD: "SoundCloud",
+} as const;
+type ArtistLinkType = (typeof _ArtistLinkType)[keyof typeof _ArtistLinkType];
+
+interface ArtistLink {
+  type: ArtistLinkType;
+  url: string;
+}
+
+function parseArtistLinks(links: string[]): ArtistLink[] {
+  const artistLinks: ArtistLink[] = [];
+  for (const link of links) {
+    if (link.match(/facebook/)) {
+      artistLinks.push({
+        url: link,
+        type: _ArtistLinkType.FB,
+      });
+    } else if (link.match("/soundcloud")) {
+      artistLinks.push({
+        url: link,
+        type: _ArtistLinkType.SOUNDCLOUD,
+      });
+    }
+  }
+
+  return artistLinks;
+}
+
+type Props = {
+  artist: Queries.AllArtistsPageQuery["allArtistsJson"]["nodes"][0];
+  imageNodes: Queries.AllArtistsPageQuery["allImageSharp"]["nodes"];
+};
+
+const LineupArtist = ({ artist, imageNodes }: Props) => {
+  const [imageData, imageName] = findImageForArtist(artist, imageNodes);
+
+  const links = parseArtistLinks(artist.links || []);
+  const soundCloudLinkOrFirst =
+    links.find((link) => link.type === _ArtistLinkType.SOUNDCLOUD) || links[0];
+
+  return (
+    <a
+      key={artist.id}
+      className="c-lineup-artist"
+      href={soundCloudLinkOrFirst?.url}
+    >
+      <GatsbyImage
+        className="c-lineup-artist__cover"
+        image={imageData}
+        alt={imageName}
+      ></GatsbyImage>
+      <h4 className="c-lineup-artist__title">{artist.title}</h4>
+    </a>
+  );
+};
+
+export default LineupArtist;
--- src/pages/lineup/index.tsx
+++ src/pages/lineup/index.tsx
@@ -6,59 +6,9 @@ import {
   StaticImage,
 } from "gatsby-plugin-image";
 import ArtistCoverImage from "../../components/lineup/ArtistCoverImage";
+import LineupArtist from "../../components/lineup/Artist";
 
 type Props = {};
-
-function findImageForArtist(
-  artist: Queries.AllArtistsPageQuery["allArtistsJson"]["nodes"][0],
-  images: Queries.AllArtistsPageQuery["allImageSharp"]["nodes"]
-): [IGatsbyImageData, string] {
-  const imageName = artist.images?.[0];
-
-  if (!imageName) {
-    throw new Error(`Artist ${artist?.id} has no image assigned `);
-  }
-
-  const imageNode = images.find((image) =>
-    imageName.includes(image.fixed?.originalName!)
-  );
-
-  if (!imageNode?.gatsbyImageData) {
-    throw new Error(`Failed to find image with name ${imageName}`);
-  }
-
-  return [imageNode.gatsbyImageData, imageName];
-}
-
-const _ArtistLinkType = {
-  FB: "Facebook",
-  SOUNDCLOUD: "SoundCloud",
-} as const;
-type ArtistLinkType = (typeof _ArtistLinkType)[keyof typeof _ArtistLinkType];
-
-interface ArtistLink {
-  type: ArtistLinkType;
-  url: string;
-}
-
-function parseArtistLinks(links: string[]): ArtistLink[] {
-  const artistLinks: ArtistLink[] = [];
-  for (const link of links) {
-    if (link.match(/facebook/)) {
-      artistLinks.push({
-        url: link,
-        type: _ArtistLinkType.FB,
-      });
-    } else if (link.match("/soundcloud")) {
-      artistLinks.push({
-        url: link,
-        type: _ArtistLinkType.SOUNDCLOUD,
-      });
-    }
-  }
-
-  return artistLinks;
-}
 
 const LineupPage = ({ data }: PageProps<Queries.AllArtistsPageQuery>) => {
   return (
@@ -67,32 +17,12 @@ const LineupPage = ({ data }: PageProps<Queries.AllArtistsPageQuery>) => {
       <section id="mainStage" className="c-page-lineup__stage">
         <h2>Main Stage</h2>
         <div className="c-page-lineup__list">
-          {data.allArtistsJson.nodes.map((artist) => {
-            const [imageData, imageName] = findImageForArtist(
-              artist,
-              data.allImageSharp.nodes
-            );
-
-            const links = parseArtistLinks(artist.links || []);
-            const soundCloudLinkOrFirst =
-              links.find((link) => link.type === _ArtistLinkType.SOUNDCLOUD) ||
-              links[0];
-
-            return (
-              <a
-                key={artist.id}
-                className="c-lineup-artist"
-                href={soundCloudLinkOrFirst?.url}
-              >
-                <GatsbyImage
-                  className="c-lineup-artist__cover"
-                  image={imageData}
-                  alt={imageName}
-                ></GatsbyImage>
-                <h4 className="c-lineup-artist__title">{artist.title}</h4>
-              </a>
-            );
-          })}
+          {data.allArtistsJson.nodes.map((artist) => (
+            <LineupArtist
+              artist={artist}
+              imageNodes={data.allImageSharp.nodes}
+            />
+          ))}
         </div>
       </section>
     </div>
