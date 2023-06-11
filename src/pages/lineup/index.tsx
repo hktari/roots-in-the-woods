134e1471f23c736da669a47ef40cc7f46e969701
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-06-11 12:24:06 +0200

    refactor findImageForArtist

--- src/pages/lineup/index.tsx
+++ src/pages/lineup/index.tsx
@@ -12,24 +12,29 @@ type Props = {};
 function findImageForArtist(
   artist: Queries.AllArtistsPageQuery["allArtistsJson"]["nodes"][0],
   images: Queries.AllArtistsPageQuery["allImageSharp"]["nodes"]
-): IGatsbyImageData | undefined {
+): [IGatsbyImageData, string] {
   const imageName = artist.images?.[0];
 
   if (!imageName) {
-    throw new Error(`No image found for artist ${artist?.id}`);
+    throw new Error(`Artist ${artist?.id} has no image assigned `);
   }
 
   const imageNode = images.find((image) =>
     imageName.includes(image.fixed?.originalName!)
   );
-  return imageNode?.gatsbyImageData;
+
+  if (!imageNode?.gatsbyImageData) {
+    throw new Error(`Failed to find image with name ${imageName}`);
+  }
+
+  return [imageNode.gatsbyImageData, imageName];
 }
 
 const _ArtistLinkType = {
   FB: "Facebook",
   SOUNDCLOUD: "SoundCloud",
 } as const;
-type ArtistLinkType = typeof _ArtistLinkType[keyof typeof _ArtistLinkType];
+type ArtistLinkType = (typeof _ArtistLinkType)[keyof typeof _ArtistLinkType];
 
 interface ArtistLink {
   type: ArtistLinkType;
@@ -63,21 +68,26 @@ const LineupPage = ({ data }: PageProps<Queries.AllArtistsPageQuery>) => {
         <h2>Main Stage</h2>
         <div className="c-page-lineup__list">
           {data.allArtistsJson.nodes.map((artist) => {
-            const imageData = findImageForArtist(
+            const [imageData, imageName] = findImageForArtist(
               artist,
               data.allImageSharp.nodes
             );
+
+            const links = parseArtistLinks(artist.links || []);
+            const soundCloudLinkOrFirst =
+              links.find((link) => link.type === _ArtistLinkType.SOUNDCLOUD) ||
+              links[0];
 
             return (
               <a
                 key={artist.id}
                 className="c-lineup-artist"
-                href={artist.link!}
+                href={soundCloudLinkOrFirst?.url}
               >
                 <GatsbyImage
                   className="c-lineup-artist__cover"
                   image={imageData}
-                  alt={artist.img}
+                  alt={imageName}
                 ></GatsbyImage>
                 <h4 className="c-lineup-artist__title">{artist.title}</h4>
               </a>
