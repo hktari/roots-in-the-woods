7f8c7e04cf0d99c720135edb7652f0d58d287b86
Boštjan Kamnik <bkamnik1995@gmail.com>
2023-06-12 17:11:13 +0200

    update banner

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,29 +1,42 @@
-import React, { useState } from 'react'
-import NavBar from '../navbar/navbar'
-import headerImage from '../../images/roots-banner-2023.jpg'
-import SideBar from '../sidebar'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import NavBar from "../navbar/navbar";
+import headerImageDesktop from "../../data/images/roots-banner-desktop.jpg";
+import headerImageMobile from "../../data/images/roots-banner-mobile.jpg";
+//
+import SideBar from "../sidebar";
+import { useEffect } from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 type HeaderProps = {
-    setDisableScroll: (disabled: boolean) => void
-}
+  setDisableScroll: (disabled: boolean) => void;
+};
 
 const Header = ({ setDisableScroll }: HeaderProps) => {
-    const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    useEffect(() => {
-        setDisableScroll(sidebarOpen)
-    }, [sidebarOpen])
+  useEffect(() => {
+    setDisableScroll(sidebarOpen);
+  }, [sidebarOpen]);
 
-    return (
-        <>
-            <header className='c-header'>
-                <img className='c-header__img' src={headerImage} alt="" />
-            </header>
-            <SideBar isOpen={sidebarOpen} closeMenuClicked={() => setSidebarOpen(false)} />
-            <NavBar openMenuClicked={() => setSidebarOpen(true)} />
-        </>
-    )
-}
+  return (
+    <>
+      <header className="c-header">
+        <img
+          className="c-header__img"
+          srcSet={`${headerImageMobile} 1219w, ${headerImageDesktop} 1920w`}
+          sizes="(max-width: 768px) 100vw,
+          1920px"
+          src={headerImageDesktop}
+          alt="Roots In The Woods Banner"
+        />
+      </header>
+      <SideBar
+        isOpen={sidebarOpen}
+        closeMenuClicked={() => setSidebarOpen(false)}
+      />
+      <NavBar openMenuClicked={() => setSidebarOpen(true)} />
+    </>
+  );
+};
 
-export default Header
+export default Header;
