0666aa3133f491fe21b077fb2dedfda9b0a9cd9d
hktari <bkamnik1995@gmail.com>
2023-12-06 11:53:55 +0100

    add 'coming soon'

--- src/components/index/activities-section.tsx
+++ src/components/index/activities-section.tsx
@@ -9,128 +9,145 @@ type Props = {
 };
 
 const ActivitiesSection = ({ activities }: Props) => {
+  function noActivitiesAdded() {
+    return activities.totalCount === 0;
+  }
+
+  function renderComingSoon() {
+    return "Coming soon...";
+  }
+  function renderActivites() {
+    return (
+      <>
+      {/* desktop */}
+        <div
+          id="carouselDesktop"
+          className="carousel slide d-none d-md-block"
+          data-bs-ride="carousel"
+          data-bs-pause="false"
+        >
+          <div className="carousel-inner" data-bs-ride="carousel">
+            {
+              // make groups of two
+              activities.edges
+                .reduce((group: any[], edge, idx) => {
+                  const groupIdx = idx % 2;
+                  group[`${groupIdx}`] = group[`${groupIdx}`] ?? [];
+                  group[`${groupIdx}`].push(edge.node);
+                  return group;
+                }, [])
+                .map((group: any, groupIdx) => (
+                  <div
+                    key={groupIdx}
+                    className={`carousel-item ${
+                      groupIdx === 0 ? "active" : ""
+                    }`}
+                    data-bs-interval="6000"
+                  >
+                    <div className="row">
+                      {group.map(
+                        (
+                          activity: PageProps<Queries.IndexPageQuery>["data"]["activities"]["edges"][0]["node"]
+                        ) => (
+                          <div key={activity.title} className="col-6">
+                            <ActivityItem
+                              title={activity.title!}
+                              description={activity.description!}
+                              image={activity.image?.gatsbyImageData!}
+                            />
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </div>
+                ))
+            }
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselDesktop"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselDesktop"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+
+        {/* mobile */}
+        <div className="row">
+          <div className="col px-0">
+            <div
+              id="carouselMobile"
+              className="carousel slide d-md-none"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner " data-bs-ride="carousel">
+                {activities.edges.map((edge, idx) => (
+                  <div
+                    key={edge.node.title}
+                    className={`carousel-item ${idx === 0 ? "active" : ""}`}
+                    data-bs-interval="3500"
+                  >
+                    <ActivityItem
+                      title={edge.node.title!}
+                      description={edge.node.description!}
+                      image={edge.node.image?.gatsbyImageData!}
+                    />
+                  </div>
+                ))}
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselMobile"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselMobile"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <section className="c-section">
       <h2 className="c-section__title">Activities</h2>
 
-      {/* desktop */}
-      <div
-        id="carouselDesktop"
-        className="carousel slide d-none d-md-block"
-        data-bs-ride="carousel"
-        data-bs-pause="false"
-      >
-        <div className="carousel-inner" data-bs-ride="carousel">
-          {
-            // make groups of two
-            activities.edges
-              .reduce((group: any[], edge, idx) => {
-                const groupIdx = idx % 2;
-                group[`${groupIdx}`] = group[`${groupIdx}`] ?? [];
-                group[`${groupIdx}`].push(edge.node);
-                return group;
-              }, [])
-              .map((group: any, groupIdx) => (
-                <div
-                  key={groupIdx}
-                  className={`carousel-item ${groupIdx === 0 ? "active" : ""}`}
-                  data-bs-interval="6000"
-                >
-                  <div className="row">
-                    {group.map(
-                      (
-                        activity: PageProps<Queries.IndexPageQuery>["data"]["activities"]["edges"][0]["node"]
-                      ) => (
-                        <div key={activity.title} className="col-6">
-                          <ActivityItem
-                            title={activity.title!}
-                            description={activity.description!}
-                            image={activity.image?.gatsbyImageData!}
-                          />
-                        </div>
-                      )
-                    )}
-                  </div>
-                </div>
-              ))
-          }
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselDesktop"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselDesktop"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-
-      {/* mobile */}
-      <div className="row">
-        <div className="col px-0">
-          <div
-            id="carouselMobile"
-            className="carousel slide d-md-none"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner " data-bs-ride="carousel">
-              {activities.edges.map((edge, idx) => (
-                <div
-                  key={edge.node.title}
-                  className={`carousel-item ${idx === 0 ? "active" : ""}`}
-                  data-bs-interval="3500"
-                >
-                  <ActivityItem
-                    title={edge.node.title!}
-                    description={edge.node.description!}
-                    image={edge.node.image?.gatsbyImageData!}
-                  />
-                </div>
-              ))}
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselMobile"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselMobile"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
-        </div>
-      </div>
+      {noActivitiesAdded() ? renderComingSoon() : renderActivites()}
     </section>
   );
 };
