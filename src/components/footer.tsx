6c8a4dabed872bc65e1430af103c1580da35ea94
hktari <bkamnik1995@gmail.com>
2023-12-07 09:53:26 +0100

    implement sticky footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className='mt-5 bg-dark p-2 p-md-4 d-flex flex-row justify-content-center align-items-center'>
+    <footer className='bg-dark p-2 p-md-4 d-flex flex-row justify-content-center align-items-center'>
         <small className='text-muted'>Roots in the Woods</small>
         <span className='mx-2 text-muted'>|</span>
         <a className='text-decoration-none text-muted' href="https://www.bostjankamnik.com"><small>bkamnik 2023</small></a>
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Header from '../header/header'
+import React from "react";
+import Header from "../header/header";
 
-import '../../css/main.scss'
-import Footer from '../footer'
-import { useState } from 'react'
+import "../../css/main.scss";
+import Footer from "../footer";
+import { useState } from "react";
 
 type Props = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const Layout = ({ children }: Props) => {
+  const [disableScroll, setDisableScroll] = useState(false);
 
-    const [disableScroll, setDisableScroll] = useState(false)
+  return (
+    <main className={`c-page ${disableScroll ? "c-page--no-scroll" : ""}`}>
+      <div className="c-page__content container-md pb-5">
+        <Header setDisableScroll={setDisableScroll} />
+        {children}
+      </div>
+      <div className="c-page__footer">
+        <Footer />
+      </div>
+    </main>
+  );
+};
 
-    return (
-        <main className={`c-page ${disableScroll ? 'c-page--no-scroll' : ''}`}>
-            <Header setDisableScroll={setDisableScroll}/>
-            <div className="container-md">
-                {children}
-            </div>
-
-            <Footer />
-        </main>
-    )
-}
-
-export default Layout
+export default Layout;
