9590baadd1b18284ee8191551c6e8e414df1bd8a
hktari <bkamnik1995@gmail.com>
2023-12-08 10:36:49 +0100

    layout gallery page

---
+++ src/pages/gallery/index.tsx
@@ -0,0 +1,67 @@
+import { Link, PageProps, graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
+import React from "react";
+import { makeGroupsOf } from "../../util/format";
+
+type Props = {};
+
+const GalleryPage = ({ data }: PageProps<Queries.GalleryPageQuery>) => {
+  const GalleryItem = ({
+    id,
+    title,
+    bannerMobile,
+  }: Queries.GalleryPageQuery["allContentfulGallery"]["edges"][0]["node"]) => {
+    return (
+      <Link to={`/gallery/${id}`}>
+        <div className="card text-bg-dark">
+          <GatsbyImage
+            image={bannerMobile?.gatsbyImageData!}
+            className="card-img"
+            alt={bannerMobile?.title!}
+          />
+        </div>
+      </Link>
+    );
+  };
+
+  return (
+    <div className="container">
+      <h1 className="c-page__title">Gallery</h1>
+
+      {makeGroupsOf(data.allContentfulGallery.edges, 3).map((groups) => (
+        <div className="row">
+          {groups.map((edge) => (
+            <div className="col col-md-4">
+              <GalleryItem {...edge.node} />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GalleryPage;
+
+export const query = graphql`
+  query GalleryPage {
+    allContentfulGallery {
+      edges {
+        node {
+          bannerMobile {
+            gatsbyImageData(width: 432)
+            title
+          }
+          description {
+            description
+          }
+          eventDate
+          eventDateEnd
+          facebookAlbumUrl
+          id
+          title
+        }
+      }
+    }
+  }
+`;
