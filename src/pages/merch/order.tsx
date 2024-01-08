8b09cf11467f80eb0de7f133ba1800b5f6c3175a
hktari <bkamnik1995@gmail.com>
2024-01-08 16:26:02 +0100

    update order page instructions

--- src/pages/merch/order.tsx
+++ src/pages/merch/order.tsx
@@ -6,13 +6,7 @@ const OrderPage = (props: Props) => {
   return (
     <div className="container">
       <h1 className="c-page__title my-md-4">Order Instructions</h1>
-      <p className="fs-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias autem nam nihil odio sequi labore libero quod illo praesentium tempora mollitia omnis quia debitis harum repellat, suscipit optio numquam illum!</p>
-      <ol>
-        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, totam!</li>
-        <li>Voluptatum quidem commodi nobis, fuga voluptas molestiae aliquam eveniet quam?</li>
-        <li>Nam aspernatur praesentium quisquam iure sapiente recusandae delectus quam consectetur!</li>
-        <li>Animi harum tenetur amet voluptatem totam, consectetur dolorum. Natus, porro.</li>
-      </ol>
+      <p className="fs-4">Please contact the following mobile number to make an order <a href="tel:+38651246140">+38651246140</a></p>
     </div>
   );
 };
