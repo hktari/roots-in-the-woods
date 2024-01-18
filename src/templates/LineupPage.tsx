4ae02818c262b769bf2dc98c9fa99796389a5764
hktari <bkamnik1995@gmail.com>
2024-01-18 10:18:48 +0100

    install lodash and group by artist stage

--- src/templates/LineupPage.tsx
+++ src/templates/LineupPage.tsx
@@ -1,28 +1,85 @@
 import { PageProps, graphql, useStaticQuery } from "gatsby";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 import React from "react";
+import LineupStage from "../components/lineup/Stage";
+import _groupBy from "lodash/groupBy";
 
 type Props = {};
 
-const LineupPage = (props: Props) => {
-  // TODO: use static query
-  const data = useStaticQuery(graphql`
-    query LineupDetailPage {
-      contentfulLineup(id: { eq: "55f6fdd8-5af3-582a-90dd-6f2b092f4425" }) {
-        artists {
-          countryOfOrigin
-          name
-          photo {
-            gatsbyImageData
-          }
-          socialMediaLink
-          stage
-        }
-        id
-        name
-      }
-    }
-  `);
+const LineupPage = ({ data }: PageProps<Queries.LineupDetailPageQuery>) => {
+  const noImagePlaceholder: IGatsbyImageData =
+    data.noImagePlaceholder.nodes[0].gatsbyImageData;
 
-  return <div>{JSON.stringify(data)}</div>;
+  const artistsByStage = _groupBy(
+    data.contentfulLineup?.artists,
+    (artist) => artist?.stage
+  );
+
+  return (
+    <div className="c-page-lineup">
+      <h1 className="c-page__title">2023 Lineup</h1>
+      {JSON.stringify(artistsByStage)}
+      {/*<section id="mainStage" className="c-page-lineup__stage">
+
+         <LineupStage
+          title="Main Stage"
+          artistNodes={data.contentfulLineup?.artists.filter(
+            (artist) => artist.stage === "main_stage"
+          )}
+          imageNodes={data.allImageSharp.nodes}
+          noImagePlaceholder={noImagePlaceholder}
+        />
+      </section>
+      <section id="dubStage" className="c-page-lineup__stage">
+        <LineupStage
+          title="Dub Stage"
+          artistNodes={data.allArtistsJson.nodes.filter(
+            (artist) => artist.stage === "dub_stage"
+          )}
+          imageNodes={data.allImageSharp.nodes}
+          noImagePlaceholder={noImagePlaceholder}
+        />
+      </section>
+      <section id="jungleDnbStage" className="c-page-lineup__stage">
+        <LineupStage
+          title="Jungle / Drum & Bass Stage"
+          artistNodes={data.allArtistsJson.nodes.filter(
+            (artist) => artist.stage === "jungle_dnb_stage"
+          )}
+          imageNodes={data.allImageSharp.nodes}
+          noImagePlaceholder={noImagePlaceholder}
+        />
+      </section> */}
+    </div>
+  );
 };
 export default LineupPage;
+
+export const query = graphql`
+  query LineupDetailPage($id: String!) {
+    contentfulLineup(id: { eq: $id }) {
+      artists {
+        countryOfOrigin
+        name
+        photo {
+          gatsbyImageData
+        }
+        socialMediaLink
+        stage
+      }
+      id
+      name
+    }
+
+    noImagePlaceholder: allImageSharp(
+      filter: { fixed: { originalName: { regex: "/logo/" } } }
+    ) {
+      nodes {
+        gatsbyImageData(width: 512)
+        fixed {
+          originalName
+        }
+      }
+    }
+  }
+`;
