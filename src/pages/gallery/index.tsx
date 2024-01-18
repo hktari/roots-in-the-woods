ab5e59ed344b4771e925aaf99ff7e07d57eeb385
hktari <bkamnik1995@gmail.com>
2024-01-18 09:57:03 +0100

    add title to gallery item and adjust style

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -12,13 +12,18 @@ const GalleryPage = ({ data }: PageProps<Queries.GalleryPageQuery>) => {
     bannerMobile,
   }: Queries.GalleryPageQuery["allContentfulGallery"]["edges"][0]["node"]) => {
     return (
-      <Link to={`/gallery/${id}`}>
-        <div className="card text-bg-dark">
-          <GatsbyImage
-            image={bannerMobile?.gatsbyImageData!}
-            className="card-img"
-            alt={bannerMobile?.title!}
-          />
+      <Link to={`/gallery/${id}`} className="text-decoration-none">
+        <div className="card rounded-0">
+          <div className="card-header">
+            <h3 className="text-black">{title}</h3>
+          </div>
+          <div className="">
+            <GatsbyImage
+              image={bannerMobile?.gatsbyImageData!}
+              className="card-img"
+              alt={bannerMobile?.title!}
+            />
+          </div>
         </div>
       </Link>
     );
@@ -26,7 +31,7 @@ const GalleryPage = ({ data }: PageProps<Queries.GalleryPageQuery>) => {
 
   return (
     <div className="container">
-      <h1 className="c-page__title mb-2">Gallery</h1>
+      <h1 className="c-page__title mb-4">Gallery</h1>
 
       {makeGroupsOf(data.allContentfulGallery.edges, 3).map((groups) => (
         <div className="row">
