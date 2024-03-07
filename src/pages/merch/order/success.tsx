0a464c90d103ff7790c544371718b153f4b00839
hktari <bkamnik1995@gmail.com>
2024-03-07 12:33:09 +0100

    edit style of success page

--- src/pages/merch/order/success.tsx
+++ src/pages/merch/order/success.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialList from "../../../components/social-list";
 
 type Props = {};
 
@@ -8,9 +9,18 @@ const Success = (props: Props) => {
       <h1 className="c-page__title my-md-4 mb-4">Order Successful</h1>
 
       <p className="fs-5">
-        Thank you for placing an order for our awesome merchandise ! <br />
-        We will get back to you as soon as we process the order.
+        <span className="fs-3">
+          Thank you for ordering our awesome merchandise !
+        </span>
+        <br />
+        <br />
+        We will get back to you as soon as we process the order. Feel free to
+        reach out to us should you have any questions.
       </p>
+
+      <div className="mt-5">
+        <SocialList />
+      </div>
     </div>
   );
 };
