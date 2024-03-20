9fa3cd5dfa72f66300de0ae8f1a1a6fa8eeba2d6
hktari <bkamnik1995@gmail.com>
2024-03-20 16:31:01 +0100

    productCard update layout

--- src/components/products/productCard.js
+++ src/components/products/productCard.js
@@ -2,56 +2,14 @@ import React, { useState } from "react";
 import getStripe from "../../utils/stripejs";
 import { useShoppingCart, formatCurrencyString } from "use-shopping-cart";
 
-const cardStyles = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "space-around",
-  alignItems: "flex-start",
-  padding: "1rem",
-  marginBottom: "1rem",
-  boxShadow: "5px 5px 25px 0 rgba(46,61,73,.2)",
-  backgroundColor: "#fff",
-  borderRadius: "6px",
-  maxWidth: "300px",
-};
-const buttonStyles = {
-  display: "block",
-  fontSize: "13px",
-  textAlign: "center",
-  color: "#000",
-  padding: "12px",
-  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
-  backgroundColor: "rgb(255, 178, 56)",
-  borderRadius: "6px",
-  letterSpacing: "1.5px",
-};
-
-const buttonDisabledStyles = {
-  opacity: "0.5",
-  cursor: "not-allowed",
-};
-
-const formatPrice = (amount, currency) => {
-  let price = (amount / 100).toFixed(2);
-  let numberFormat = new Intl.NumberFormat(["en-US"], {
-    style: "currency",
-    currency: currency,
-    currencyDisplay: "symbol",
-  });
-  return numberFormat.format(price);
-};
-
 const ProductCard = ({ product }) => {
   const { redirectToCheckout } = useShoppingCart();
-  const { name, images, description, currency } = product;
-  const productPrice = product.prices[0];
+  const { name, image, description, currency, price } = product;
   const formattedPrice = formatCurrencyString({
-    value: productPrice.unit_amount,
-    currency: productPrice.currency,
+    value: price,
+    currency: currency,
     language: "en-US",
   });
-
-  async function buyNow() {}
 
   const [loading, setLoading] = useState(false);
 
@@ -81,26 +39,30 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div style={cardStyles}>
-      <form onSubmit={handleSubmit}>
-        <fieldset style={{ border: "none" }}>
-          <legend>
-            <h4>{name}</h4>
-          </legend>
-          <label>Price: {formattedPrice} </label>
-        </fieldset>
-        <button
-          disabled={loading}
-          style={
-            loading
-              ? { ...buttonStyles, ...buttonDisabledStyles }
-              : buttonStyles
-          }
-        >
-          BUY ME
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={image} className="card-img-top" alt={name} />
+        <div className="card-body">
+          <h5 className="card-title">{name}</h5>
+          <h6> {formattedPrice}</h6>
+          <input
+            name="amount"
+            class="form-control"
+            type="number"
+            placeholder="0"
+            max={10}
+            min={1}
+            required
+            aria-label="amount"
+          />
+
+          <p className="card-text">{description}</p>
+          <button disabled={loading} href="#" className="btn btn-primary">
+            BUY
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
