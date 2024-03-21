980b7c7826b99fca9451cd2fc47db9814d2696d7
hktari <bkamnik1995@gmail.com>
2024-03-21 10:29:32 +0100

    layout shopping cart item

--- src/components/shopping-cart/shopping-cart-item.tsx
+++ src/components/shopping-cart/shopping-cart-item.tsx
@@ -5,13 +5,15 @@ type Props = { item: any };
 const ShoppingCartItem = ({ item }: Props) => {
   const { image, name, quantity, formattedPrice } = item;
   return (
-    <div className="list-group-item d-flex bg-danger">
+    <div className="list-group-item d-flex bg-secondary align-items-center">
       <img height={100} src={image} alt={name} />
-      <div className="d-flex w-100 justify-content-between">
-        <h5 className="mb-1">{quantity}x {name}</h5>
-      </div>
-      <p className="mb-1">{formattedPrice}}</p>
-      <small>And some small print.</small>
+      <span className="fs-4 flex-grow-1">
+        {quantity}x {name}
+      </span>
+      <span className="ms-5 fs-4">{formattedPrice}</span>
+      <button type="button" className="btn btn-close ms-1" aria-label="Close">
+        <i className="bi bi-x fs-2"></i>
+      </button>
     </div>
   );
 };
