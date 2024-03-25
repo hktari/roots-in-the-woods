2e2122afeea9e11e00e6bff7f4fdda557334b83e
hktari <bkamnik1995@gmail.com>
2024-03-25 11:36:37 +0100

    style shopping cart oggle button

--- src/components/shopping-cart/shopping-cart.tsx
+++ src/components/shopping-cart/shopping-cart.tsx
@@ -32,20 +32,40 @@ const ShoppingCart = (props: Props) => {
     redirectToCheckout(response.sessionId);
   };
 
+  const isCollapsed = true;
   return (
-    <div className="c-shopping-cart list-group me-5" hidden={isEmpty}>
-      {Object.keys(cartDetails).map((itemId) => (
-        <ShoppingCartItem item={cartDetails[itemId]} />
-      ))}
+    <div
+      className={`c-shopping-cart ${
+        isCollapsed ? "c-shopping-cart--collapsed" : ""
+      }`}
+      hidden={isEmpty}
+    >
+      <div className="c-shopping-cart--container">
+        <div className="list-group me-5">
+          {Object.keys(cartDetails).map((itemId) => (
+            <ShoppingCartItem item={cartDetails[itemId]} />
+          ))}
+        </div>
+        <div className="text-center mt-2">
+          <button
+            disabled={isLoading}
+            className="btn btn-primary btn-lg text-white"
+            onClick={onCheckout}
+          >
+            CHECKOUT
+          </button>
+        </div>
+      </div>
 
-      <div className="text-center mt-2">
-        <button
-          disabled={isLoading}
-          className="btn btn-primary btn-lg text-white"
-          onClick={onCheckout}
-        >
-          CHECKOUT
+      <div className="c-shopping-cart--toggle">
+        <button className="btn bg-dark rounded-circle">
+          <i className="bi bi-cart4 text-white fs-1"></i>
         </button>
+        <div className="c-shopping-cart--toggle-counter-bg rounded-circle ">
+          <span className="c-shopping-cart--toggle-counter badge text-bg-primary text-white">
+            {cartCount}
+          </span>
+        </div>
       </div>
     </div>
   );
