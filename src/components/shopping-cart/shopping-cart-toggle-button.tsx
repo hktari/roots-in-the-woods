82116c838bd308c9ea95c8260c6449828f06e0f5
hktari <bkamnik1995@gmail.com>
2024-03-25 16:56:09 +0100

    early return on desktop via 'useViewport'

--- src/components/shopping-cart/shopping-cart-toggle-button.tsx
+++ src/components/shopping-cart/shopping-cart-toggle-button.tsx
@@ -1,17 +1,25 @@
 import React, { useContext, useEffect } from "react";
 import { useShoppingCart } from "use-shopping-cart";
 import { LayoutContext } from "../layout/layout";
+import useViewport from "../../hooks/use-viewport";
 
 type Props = {};
 
 const ShoppingCartToggleButton = ({}: Props) => {
   const { cartCount, shouldDisplayCart, handleCartClick } = useShoppingCart();
 
+  const { width } = useViewport();
+  const bootstrapMDBreakpoint = 768;
+
   const { setDisableScroll } = useContext(LayoutContext);
   useEffect(() => {
+    if (width >= bootstrapMDBreakpoint) {
+      return;
+    }
+
     setDisableScroll(Boolean(shouldDisplayCart));
-  }, [shouldDisplayCart]);
-  
+  }, [shouldDisplayCart, width]);
+
   return (
     <div
       className={`c-shopping-cart-toggle-button ${
