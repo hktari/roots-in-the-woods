3e6b2c6b35182e5b47d6c527533e567c59c38dea
hktari <bkamnik1995@gmail.com>
2024-03-26 07:57:46 +0100

    disable -ssr.js, use wrapPageElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,20 +3,10 @@ import "@popperjs/core/dist/umd/popper.min.js";
 
 import * as React from "react";
 import Layout from "./src/components/layout/layout";
-import { CartProvider } from "use-shopping-cart";
 
-export const wrapRootElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
 
-  return (
-    <CartProvider
-      cartMode="checkout-session"
-      shouldPersist
-      stripe={process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}
-      currency="EUR"
-    >
-      <Layout {...props}>{element}</Layout>
-    </CartProvider>
-  );
+  return <Layout {...props}>{element}</Layout>;
 };
