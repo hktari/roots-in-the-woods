5cc33edd139034641b168ad8dbabb572363b4ddf
hktari <bkamnik1995@gmail.com>
2024-03-26 09:12:20 +0100

    check for window

--- src/hooks/use-viewport.tsx
+++ src/hooks/use-viewport.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
+const isBrowser = typeof window !== "undefined";
+
 const useViewport = () => {
-  const [width, setWidth] = useState(0);
+  const [width, setWidth] = useState(isBrowser ? window.innerWidth : 0);
 
   useEffect(() => {
     const handleWindowResize = () => setWidth(window.innerWidth);
