ddf47e4bb6ce5dda5706d1ec20b1860fb667595a
hktari <bkamnik1995@gmail.com>
2024-08-01 10:13:37 +0200

    type facebook album response

--- src/components/gallery/PhotoAlbum.tsx
+++ src/components/gallery/PhotoAlbum.tsx
@@ -1,30 +1,20 @@
 import { PageProps } from "gatsby";
 import React from "react";
+import { AlbumsDatum, PhotosDatum, WebpImage } from "../../interface/albums";
 
-type Props = {};
-
-type Photo = {
-  id: string;
-  webp_images: PhotoSource[];
+type Props = {
+  album: AlbumsDatum;
 };
 
-type PhotoSource = {
-  source: string;
-  height: number;
-  width: number;
-};
-
-const PhotoAlbum = ({
-  album,
-}: PageProps<Queries.GalleryDetailPageQuery>["data"]["albums"]["nodes"][0]["data"][0]) => {
+const PhotoAlbum = ({ album }: Props) => {
   const MinPhotoWidth = 432;
 
-  const sortByWidthAscending = (photos: PhotoSource[]) => {
+  const sortByWidthAscending = (photos: WebpImage[]) => {
     const copy = [...photos];
     return copy.sort((a, b) => a.width - b.width);
   };
 
-  const takePhotoWithAtLeast = (minWidth: number, photos: PhotoSource[]) => {
+  const takePhotoWithAtLeast = (minWidth: number, photos: WebpImage[]) => {
     if (photos.length === 0) {
       throw new Error("photos length is zero");
     }
@@ -38,15 +28,16 @@ const PhotoAlbum = ({
     return sortedPhotos[sortedPhotos.length - 1];
   };
 
-  const takeLargestPhoto = (photos: PhotoSource[]) => {
+  const takeLargestPhoto = (photos: WebpImage[]) => {
     const sortedPhotos = sortByWidthAscending(photos);
     return sortedPhotos[sortedPhotos.length - 1];
   };
 
+  const hasNextPage = album?.photos?.paging?.next;
+
   return (
     <div className="col px-0 c-photo-album">
-
-      {album.photos?.data?.map((photo: Photo) => {
+      {album.photos?.data?.map((photo: PhotosDatum) => {
         const largestPhoto = takeLargestPhoto(photo.webp_images);
         const optimalSizePhoto = takePhotoWithAtLeast(
           MinPhotoWidth,
@@ -54,6 +45,7 @@ const PhotoAlbum = ({
         );
 
         return (
+          // TODO: extract to a separate component
           <div className="c-photo-album__img-container">
             <a href={largestPhoto.source}>
               <img src={optimalSizePhoto.source} alt={photo?.id} />
