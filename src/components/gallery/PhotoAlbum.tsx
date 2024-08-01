1a60199d91451114ec227baba733472ac8423f33
hktari <bkamnik1995@gmail.com>
2024-08-01 10:17:31 +0200

    extract PhotoAlbumItem component

--- src/components/gallery/PhotoAlbum.tsx
+++ src/components/gallery/PhotoAlbum.tsx
@@ -1,58 +1,18 @@
 import { PageProps } from "gatsby";
 import React from "react";
 import { AlbumsDatum, PhotosDatum, WebpImage } from "../../interface/albums";
+import PhotoAlbumItem from "./photoAlbumItem";
 
 type Props = {
   album: AlbumsDatum;
 };
 
 const PhotoAlbum = ({ album }: Props) => {
-  const MinPhotoWidth = 432;
-
-  const sortByWidthAscending = (photos: WebpImage[]) => {
-    const copy = [...photos];
-    return copy.sort((a, b) => a.width - b.width);
-  };
-
-  const takePhotoWithAtLeast = (minWidth: number, photos: WebpImage[]) => {
-    if (photos.length === 0) {
-      throw new Error("photos length is zero");
-    }
-
-    const sortedPhotos = sortByWidthAscending(photos);
-    for (let i = 0; i < sortedPhotos.length; i++) {
-      if (sortedPhotos[i].width >= minWidth) {
-        return sortedPhotos[i];
-      }
-    }
-    return sortedPhotos[sortedPhotos.length - 1];
-  };
-
-  const takeLargestPhoto = (photos: WebpImage[]) => {
-    const sortedPhotos = sortByWidthAscending(photos);
-    return sortedPhotos[sortedPhotos.length - 1];
-  };
-
   const hasNextPage = album?.photos?.paging?.next;
 
   return (
     <div className="col px-0 c-photo-album">
-      {album.photos?.data?.map((photo: PhotosDatum) => {
-        const largestPhoto = takeLargestPhoto(photo.webp_images);
-        const optimalSizePhoto = takePhotoWithAtLeast(
-          MinPhotoWidth,
-          photo.webp_images
-        );
-
-        return (
-          // TODO: extract to a separate component
-          <div className="c-photo-album__img-container">
-            <a href={largestPhoto.source}>
-              <img src={optimalSizePhoto.source} alt={photo?.id} />
-            </a>
-          </div>
-        );
-      })}
+      {album.photos?.data?.map((photo: PhotosDatum) => <PhotoAlbumItem key={photo.id} photo={photo} minPhotoWidth={432} />)}
     </div>
   );
 };
---
+++ src/components/gallery/photoAlbumItem/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { PhotosDatum, WebpImage } from "../../../interface/albums";
+
+type Props = {
+  photo: PhotosDatum;
+  minPhotoWidth: number;
+};
+
+const PhotoAlbumItem = ({ photo, minPhotoWidth }: Props) => {
+  const sortByWidthAscending = (photos: WebpImage[]) => {
+    const copy = [...photos];
+    return copy.sort((a, b) => a.width - b.width);
+  };
+
+  const takePhotoWithAtLeast = (minWidth: number, photos: WebpImage[]) => {
+    if (photos.length === 0) {
+      throw new Error("photos length is zero");
+    }
+
+    const sortedPhotos = sortByWidthAscending(photos);
+    for (let i = 0; i < sortedPhotos.length; i++) {
+      if (sortedPhotos[i].width >= minWidth) {
+        return sortedPhotos[i];
+      }
+    }
+    return sortedPhotos[sortedPhotos.length - 1];
+  };
+
+  const takeLargestPhoto = (photos: WebpImage[]) => {
+    const sortedPhotos = sortByWidthAscending(photos);
+    return sortedPhotos[sortedPhotos.length - 1];
+  };
+
+  const largestPhoto = takeLargestPhoto(photo.webp_images);
+  const optimalSizePhoto = takePhotoWithAtLeast(
+    minPhotoWidth,
+    photo.webp_images
+  );
+
+  return (
+    <div className="c-photo-album__img-container">
+      <a href={largestPhoto.source}>
+        <img src={optimalSizePhoto.source} alt={photo?.id} />
+      </a>
+    </div>
+  );
+};
+
+export default PhotoAlbumItem;
