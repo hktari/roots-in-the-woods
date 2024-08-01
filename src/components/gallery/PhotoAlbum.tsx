f8f5a2b95eb2b11a1ef2174a39558009518377fc
hktari <bkamnik1995@gmail.com>
2024-08-01 10:29:44 +0200

    implement pagination

--- src/components/gallery/PhotoAlbum.tsx
+++ src/components/gallery/PhotoAlbum.tsx
@@ -1,5 +1,5 @@
 import { PageProps } from "gatsby";
-import React from "react";
+import React, { useState } from "react";
 import { AlbumsDatum, PhotosDatum, WebpImage } from "../../interface/albums";
 import PhotoAlbumItem from "./photoAlbumItem";
 
@@ -8,11 +8,39 @@ type Props = {
 };
 
 const PhotoAlbum = ({ album }: Props) => {
-  const hasNextPage = album?.photos?.paging?.next;
+  const curPage = album.photos;
+  const [photos, setPhotos] = useState([...curPage?.data]);
+
+  const [nextPageUrl, setNextPageUrl] = useState(curPage?.paging?.next);
+
+  const hasNextPage = !!nextPageUrl;
+
+  const onLoadMore = async () => {
+    if (!hasNextPage) return;
+
+    try {
+      const res = await fetch(nextPageUrl);
+      const nextPagePhotos = (await res.json()).data;
+      setPhotos([...photos, ...nextPagePhotos]);
+      setNextPageUrl(nextPagePhotos?.paging?.next);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="col px-0 c-photo-album">
-      {album.photos?.data?.map((photo: PhotosDatum) => <PhotoAlbumItem key={photo.id} photo={photo} minPhotoWidth={432} />)}
+      {photos.map((photo: PhotosDatum) => (
+        <PhotoAlbumItem key={photo.id} photo={photo} minPhotoWidth={432} />
+      ))}
+
+      {hasNextPage && (
+        <div className="c-photo-album__load-more">
+          <button className="btn btn-primary" onClick={onLoadMore}>
+            Load More
+          </button>
+        </div>
+      )}
     </div>
   );
 };
