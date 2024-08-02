5373e82e0eceb4c52e4bc2ca6a8d010436c1a7d0
hktari <bkamnik1995@gmail.com>
2024-08-02 14:39:28 +0200

    add test to assert that images are added when

--- cypress/e2e/pages/gallery-detail.cy.ts
+++ cypress/e2e/pages/gallery-detail.cy.ts
@@ -1,12 +1,10 @@
-describe("template spec", () => {
-  beforeEach(() => {
-    cy.visit("/gallery/");
-    cy.get(".row > :nth-child(1)").click();
-  });
+describe("gallery detail page", () => {
+  context("onScrollBottom", () => {
+    beforeEach(() => {
+      cy.visit("/gallery/");
+      cy.get(".row > :nth-child(1)").click();
+    });
 
-  it("should visit page", () => {});
-
-  describe("onScrollBottom", () => {
     it("should call load more photos API", () => {
       cy.intercept({
         pathname: "/.netlify/functions/fb-albums-api",
@@ -14,6 +12,26 @@ describe("template spec", () => {
 
       cy.get(".c-photo-album__load-more").scrollIntoView();
       cy.wait("@loadMoreAPI").its("response.statusCode").should("eq", 200);
+    });
+
+    it("should increase count of photo elements", () => {
+      cy.get(".container > :nth-child(2) > :nth-child(1)")
+        .find("img")
+        .its("length")
+        .then((initialLength) => {
+          cy.intercept({
+            pathname: "/.netlify/functions/fb-albums-api",
+          }).as("loadMoreAPI");
+
+          cy.get(".c-photo-album__load-more").scrollIntoView();
+
+          cy.wait("@loadMoreAPI");
+
+          cy.get(".container > :nth-child(2) > :nth-child(1)")
+            .find("img")
+            .its("length")
+            .should("be.gt", initialLength);
+        });
     });
   });
 });
