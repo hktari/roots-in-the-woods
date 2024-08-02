c402c8a19ae70617eb17788b1c396fa62512204c
hktari <bkamnik1995@gmail.com>
2024-08-02 15:12:12 +0200

    fix onScrollBottom tests

--- cypress/e2e/pages/gallery-detail.cy.ts
+++ cypress/e2e/pages/gallery-detail.cy.ts
@@ -1,37 +1,34 @@
 describe("gallery detail page", () => {
-  context("onScrollBottom", () => {
-    beforeEach(() => {
-      cy.visit("/gallery/");
-      cy.get(".row > :nth-child(1)").click();
-    });
+  beforeEach(() => {
+    cy.visit("/gallery/");
+    cy.get(".row > :nth-child(1)").click();
+  });
 
-    it("should call load more photos API", () => {
-      cy.intercept({
-        pathname: "/.netlify/functions/fb-albums-api",
-      }).as("loadMoreAPI");
+  it("onScrollBottom should call load more photos API", () => {
+    cy.intercept({
+      pathname: "/.netlify/functions/fb-albums-api",
+    }).as("loadMoreAPI");
 
-      cy.get(".c-photo-album__load-more").scrollIntoView();
-      cy.wait("@loadMoreAPI").its("response.statusCode").should("eq", 200);
-    });
+    // wait for scroll event handler to be attached.
+    cy.wait(500);
 
-    it("should increase count of photo elements", () => {
-      cy.get(".container > :nth-child(2) > :nth-child(1)")
-        .find("img")
-        .its("length")
-        .then((initialLength) => {
-          cy.intercept({
-            pathname: "/.netlify/functions/fb-albums-api",
-          }).as("loadMoreAPI");
+    cy.get(".container > :nth-child(2) > :nth-child(1)")
+      .find("img")
+      .as("photoElements");
 
-          cy.get(".c-photo-album__load-more").scrollIntoView();
+    cy.get("@photoElements")
+      .its("length")
+      .then((initialLength) => {
+        cy.get(".c-photo-album__load-more").scrollIntoView();
 
-          cy.wait("@loadMoreAPI");
+        cy.wait("@loadMoreAPI");
 
-          cy.get(".container > :nth-child(2) > :nth-child(1)")
-            .find("img")
-            .its("length")
-            .should("be.gt", initialLength);
-        });
-    });
+        // TODO: find a way to reset the state of gallery detail page so that this assertion can be in a seperate 'it' block
+        // should increase count of photo elements
+        cy.get(".container > :nth-child(2) > :nth-child(1)")
+          .find("img")
+          .its("length")
+          .should("be.gt", initialLength);
+      });
   });
 });
