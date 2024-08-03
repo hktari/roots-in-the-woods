44eced621f1d2a231c6f2e186ef500a5cac7b7ee
hktari <bkamnik1995@gmail.com>
2024-08-03 20:05:59 +0200

    add seperate test for testing img tags are being added

--- cypress/e2e/pages/gallery-detail.cy.ts
+++ cypress/e2e/pages/gallery-detail.cy.ts
@@ -1,25 +1,27 @@
 describe("gallery detail page", () => {
   beforeEach(() => {
     cy.visit("/gallery/");
-    cy.get(".row:nth-of-type(1) > :nth-child(1)").as('firstAlbum').click();
-  });
-
-  it("onScrollBottom should call load more photos API", () => {
+    cy.get(".row:nth-of-type(1) > :nth-child(1)").as("firstAlbum").click();
     cy.intercept({
       pathname: "/.netlify/functions/fb-albums-api",
     }).as("loadMoreAPI");
+  });
 
-    // wait for scroll event handler to be attached.
-    cy.wait(500);
+  it("onScrollBottom should call load more photos API", () => {
+    // wait for photos to load
+    cy.wait(2500);
 
+    cy.get(".c-photo-album__load-more").scrollIntoView();
+
+    cy.wait("@loadMoreAPI");
+  });
+
+  it("clicking load more button should insert additional img elements", () => {
     cy.get(".container > :nth-child(2) > :nth-child(1)")
       .find("img")
-      .as("photoElements");
-
-    cy.get("@photoElements")
       .its("length")
       .then((initialLength) => {
-        cy.get(".c-photo-album__load-more").scrollIntoView();
+        cy.get(".c-photo-album__load-more").click();
 
         cy.wait("@loadMoreAPI");
 
