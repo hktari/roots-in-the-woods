8dd6951f73c617f57af3e289d4f7e19ea649e136
hktari <bkamnik1995@gmail.com>
2024-08-16 10:44:09 +0200

    query image on Map page from Contentful

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { graphql, PageProps } from "gatsby";
 
-const LineupPage = () => {
+const MapPage = ({ data }: PageProps<Queries.MapPageQuery>) => {
+  const { gatsbyImageData: mapImage, title } =
+    data?.contentfulMap?.mapImage || {};
+  if (!title || !mapImage)
+    throw new Error("Map Site not configured. Needs title and image");
+
   return (
     <>
       <h1 className="c-page__title d-md-none">Map</h1>
       <div className="py-5">
-        <StaticImage
-          src="../../data/images/map.png"
-          alt="festival map"
-          height={650}
-        />
+        <GatsbyImage image={mapImage} alt={title} />
       </div>
       <div className="d-md-none" style={{ height: "150px" }}></div>
     </>
   );
 };
 
-export default LineupPage;
+export const query = graphql`
+  query MapPage {
+    contentfulMap {
+      mapImage {
+        gatsbyImageData(layout: CONSTRAINED)
+        title
+      }
+    }
+  }
+`;
+
+export default MapPage;
