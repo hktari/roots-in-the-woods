47c3277f9e756f70e3970e6fef206e721300085a
hktari <bkamnik1995@gmail.com>
2024-12-10 13:29:47 +0100

    update get-ticket-cta component to use contentful navItem

--- src/components/get-ticket-cta.tsx
+++ src/components/get-ticket-cta.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
-import links from "../data/links";
+import { graphql, useStaticQuery } from "gatsby";
 
 type Props = {};
 
 const GetTicketCta = (props: Props) => {
+
+  const { ticketsNavItem } = useStaticQuery<TicketsNavItemQuery>(graphql`
+    query TicketsNavItem {
+      ticketsNavItem :contentfulNavigationItem(title: {regex: "/.*ticket.*/i"}) {
+        url
+      }
+  }`)
+
+  if (!ticketsNavItem) {
+    console.warn("Tickets nav item not found. Make sure a navigation item for 'tickets' exists inside Contentful");
+    console.warn("Not rendering GetTicketCta");
+    return null;
+  }
+
   return (
     <div className="c-section">
       <div className="row">
@@ -26,7 +40,7 @@ const GetTicketCta = (props: Props) => {
               ></StaticImage>
             </div>
             <button className="c-get-ticket-cta__btn c-button-link">
-              <a href={links.tickets}>GET TICKET</a>
+              <a href={ticketsNavItem.url}>GET TICKET</a>
             </button>
           </div>
         </div>
